31c3b09bc322fb1e9a619cdffe66eca9bcdcb248
reiddedeaux <rdedeaux@usf.edu>
2025-01-26 13:46:31 -0500

    Task 2 - Product details

---
+++ cc_1.js
@@ -0,0 +1,17 @@
+// Task 1
+let employeeName = "Jimmy Butler"; 
+const employeeID = 22;
+var isActive = true;
+
+console.log(typeof employeeName);
+console.log(typeof employeeID);
+console.log(typeof isActive)
+ 
+// Task 2
+let productName = "Basketball Jersey";
+const productPrice = 100;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
